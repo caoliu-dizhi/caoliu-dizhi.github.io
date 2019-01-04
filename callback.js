7feeebba3ded2cf3f2e4dec89bc4db685026d563
caoliu-dizhi <xx@xx.com>
2019-01-04 22:00:06 +0800

    + update url

--- callback.js
+++ callback.js
@@ -1,1 +1,1 @@
-callback({"url1":"hh.flexui.win","url2":"hh.etet.men","url3":"hh.dety.men","app":"NzAuZ2h1d3Mud2lu","update":"2018-11-27","note":"\u6700\u8fd1\u5730\u5740\u8b8a\u5316\u8f03\u591a\uff0c\u5982\u4e00\u6642\u7121\u6cd5\u8a2a\u554f\uff0c\u8acb\u5728\u8a2d\u7f6e\u4e2d\u6253\u958bSSL\u93c8\u63a5\u8a66\u8a66\u3002","appVer":"2.3.2"});
+callback();
